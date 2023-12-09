844707886b49d1e2387f352e8c141d838e9218b8
KGVJWijesooriya <vimukthi200020@gmail.com>
2023-12-09 14:48:26 +0530

    index

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text.", {
+    Strings:["Graphic Designer","Web Developer","UI/UX Designer","Back end Developer","Social Media Manager"],
+    typeSpeed:100,
+    backSpeed:100,
+    BackDelay:1000,
+    loop:true
+})
